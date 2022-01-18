c68bc6ca5475062473666448fa99049d92fc58d6
AdleyMD <android2026@hotmail.com>
2022-01-18 17:23:35 +0100

    Sugeno

--- Matlab/GetAngularTraining.m
+++ Matlab/GetAngularTraining.m
@@ -1,5 +1,6 @@
 train_angular = training(:,[1:6,12])
 indices = round(linspace(1,size(training,1),1500))
 train_angular = train_angular(indices,:)
-train_angular(isinf(train_angular)) = 1.0
+train_angular(isinf(train_angular)) = 5.0
+train_angular(train_angular > 5.0) = 5.0
 train_angular = double(train_angular)
--- Matlab/GetVelocityTraining.m
+++ Matlab/GetVelocityTraining.m
@@ -1,5 +1,6 @@
 train_velocity = training(:,[1:6,13])
 indices = round(linspace(1,size(training,1),1500))
 train_velocity = train_velocity(indices,:)
-train_velocity(isinf(train_velocity)) = 1.0
+train_velocity(isinf(train_velocity)) = 5.0
+train_velocity(train_velocity > 5.0) = 5.0
 train_velocity = double(train_velocity)
